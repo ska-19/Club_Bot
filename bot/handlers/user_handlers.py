0301e73198a3ec9d94112b6d5b14ee18d32260c1
ska-19 <ska04@icloud.com>
2024-01-29 19:25:57 +0700

    0.1.1

--- bot/handlers/user_handlers.py
+++ bot/handlers/user_handlers.py
@@ -1,16 +1,45 @@
 from aiogram.filters import Command
 from aiogram import Router, types
+from aiogram import F
 
 from bot.config import BotConfig
+from bot.keyboards.user_keyboards import get_main_kb, get_main_ikb, get_calc_ikb
 
 user_router = Router()
+user_data = {}
 
+async def update_num_text(message: types.Message, new_value: int):
+    await message.edit_text(
+        f"Укажите число: {new_value}",
+        reply_markup=get_calc_ikb()
+    )
+
+@user_router.message(Command("numbers"))
+async def cmd_numbers(message: types.Message):
+    user_data[message.from_user.id] = 0
+    await message.answer("Укажите число: 0", reply_markup=get_calc_ikb())
+
+@user_router.callback_query(F.data.startswith("num_"))
+async def callbacks_num(callback: types.CallbackQuery):
+    user_value = user_data.get(callback.from_user.id, 0)
+    action = callback.data.split("_")[1]
+
+    if action == "incr":
+        user_data[callback.from_user.id] = user_value+1
+        await update_num_text(callback.message, user_value+1)
+    elif action == "decr":
+        user_data[callback.from_user.id] = user_value-1
+        await update_num_text(callback.message, user_value-1)
+    elif action == "finish":
+        await callback.message.edit_text(f"Итого: {user_value}")
+
+    await callback.answer()
 
 @user_router.message(Command("reply"))
 async def cmd_reply(message: types.Message):
     """The function replies to your message"""
 
-    await message.reply('Reply message replies! 😻')
+    await message.reply('Reply message replies! 😻', reply_markup = get_main_ikb())
 
 
 @user_router.message(Command("dice"))
@@ -23,11 +52,11 @@ async def cmd_dice(message: types.Message):
 @user_router.message(Command("admin_info"))
 async def cmd_admin_info(message: types.Message, config: BotConfig):
     if message.from_user.id in config.admin_ids:
-        await message.answer("You are an admin.")
+        await message.answer(text = "You are an admin.")
     else:
         await message.answer("You are not an admin.")
 
 
 @user_router.message(Command("start"))
 async def cmd_start(message: types.Message, config: BotConfig):
-    await message.answer(config.welcome_message)
+    await message.answer(text = config.welcome_message, reply_markup = get_main_kb())
--- bot/keyboards/user_keyboards.py
+++ bot/keyboards/user_keyboards.py
@@ -1,9 +1,39 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-
-def get_main_keyboard() -> InlineKeyboardMarkup:
+def get_main_kb() -> ReplyKeyboardMarkup:
     """Get main keyboard."""
-    ikb = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton('Button_1', callback_data='callback_button_1')],
-    ])
-    return ikb
+    kb = [
+        [KeyboardButton(text="/reply")],
+        [KeyboardButton(text="/dice")],
+        [KeyboardButton(text="/numbers")],
+        [KeyboardButton(text="/admin_info")],
+        [KeyboardButton(text="/start")],
+    ]
+    keyboard = ReplyKeyboardMarkup(keyboard=kb)
+    return keyboard
+
+
+def get_main_ikb() -> InlineKeyboardMarkup:
+    """Get main keyboard."""
+    ikb = [
+        [
+            InlineKeyboardButton(text="X", callback_data="c"),
+            InlineKeyboardButton(text="Y", callback_data="cc")
+        ],
+        [InlineKeyboardButton(text="Z", callback_data="ccc")]
+    ]
+    ikeyboard = InlineKeyboardMarkup(inline_keyboard=ikb)
+    return ikeyboard
+
+def get_calc_ikb() -> InlineKeyboardMarkup:
+    """Get main keyboard."""
+    ikb = [
+        [
+            InlineKeyboardButton(text="-1", callback_data="num_decr"),
+            InlineKeyboardButton(text="+1", callback_data="num_incr")
+        ],
+        [InlineKeyboardButton(text="Подтвердить", callback_data="num_finish")]
+    ]
+    ikeyboard = InlineKeyboardMarkup(inline_keyboard=ikb)
+    return ikeyboard
