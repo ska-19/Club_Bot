0b40aaa40b8458c0fe879c58ecbbb494c495a62b
ska-19 <ska04@icloud.com>
2024-02-20 10:11:57 +0300

    FastAPI + DB template

--- src/main.py
+++ src/main.py
@@ -1,75 +1,55 @@
 from datetime import datetime
 from enum import Enum
-from typing import List, Optional
+from typing import List, Optional, Union
+
+from fastapi_users import fastapi_users, FastAPIUsers
 from pydantic import BaseModel, Field
 
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, Request, status, Depends
 from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import ResponseValidationError, RequestValidationError
+from fastapi.exceptions import RequestValidationError, ResponseValidationError, ValidationException
 from fastapi.responses import JSONResponse
+
+from src.auth.auth import auth_backend
+from src.auth.database import User
+from src.auth.manager import get_user_manager
+from src.auth.schemas import UserRead, UserCreate
 
 app = FastAPI(
     title="Trading App"
 )
 
-
-@app.exception_handler(ResponseValidationError) # RequestValidationError
-async def validation_exception_handler(request: Request, exc: ResponseValidationError):
+@app.exception_handler(ValidationException)
+async def validation_exception_handler(request: Request, exc: ValidationException):
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         content=jsonable_encoder({"detail": exc.errors()}),
     )
 
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
 
-fake_users = [
-    {"id": 1, "role": "admin", "name": ["Bob"]},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"},
-    {"id": 4, "role": "investor", "name": "Homer", "degree": [
-        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
-    ]},
-]
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
 
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
 
-class DegreeType(Enum):
-    newbie = "newbie"
-    expert = "expert"
+current_user = fastapi_users.current_user()
 
-
-class Degree(BaseModel):
-    id: int
-    created_at: datetime
-    type_degree: DegreeType
-
-
-class User(BaseModel):
-    id: int
-    role: str
-    name: str
-    degree: Optional[List[Degree]] = []
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
 
 
-@app.get("/users/{user_id}", response_model=List[User])
-def get_user(user_id: int):
-    return [user for user in fake_users if user.get("id") == user_id]
-
-
-fake_trades = [
-    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
-    {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
-]
-
-
-class Trade(BaseModel):
-    id: int
-    user_id: int
-    currency: str = Field(max_length=5)
-    side: str
-    price: float = Field(ge=0)
-    amount: float
-
-
-@app.post("/trades")
-def add_trades(trades: List[Trade]):
-    fake_trades.extend(trades)
-    return {"status": 200, "data": fake_trades}
+@app.get("/unprotected-route")
+def unprotected_route():
+    return f"Hello, anonym"
