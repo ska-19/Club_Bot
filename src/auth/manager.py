87f01d541f58b3f06793d4cce499026fae4ca7db
ska-19 <ska04@icloud.com>
2024-02-20 15:34:28 +0300

    upd template

--- src/auth/manager.py
+++ src/auth/manager.py
@@ -3,14 +3,15 @@ from typing import Optional
 from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, IntegerIDMixin, exceptions, models, schemas
 
-from src.auth.database import User, get_user_db
+from src.auth.models import User
+from src.auth.utils import get_user_db
 
-SECRET = "SECRET"
+from src.config import SECRET_AUTH
 
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
-    reset_password_token_secret = SECRET
-    verification_token_secret = SECRET
+    reset_password_token_secret = SECRET_AUTH
+    verification_token_secret = SECRET_AUTH
 
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
--- src/main.py
+++ src/main.py
@@ -1,55 +1,25 @@
-from datetime import datetime
-from enum import Enum
-from typing import List, Optional, Union
+from fastapi import FastAPI
 
-from fastapi_users import fastapi_users, FastAPIUsers
-from pydantic import BaseModel, Field
-
-from fastapi import FastAPI, Request, status, Depends
-from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import RequestValidationError, ResponseValidationError, ValidationException
-from fastapi.responses import JSONResponse
-
-from src.auth.auth import auth_backend
-from src.auth.database import User
-from src.auth.manager import get_user_manager
+from src.auth.base_config import auth_backend, fastapi_users
 from src.auth.schemas import UserRead, UserCreate
+
+from src.operations.router import router as router_operation
+
 
 app = FastAPI(
     title="Trading App"
 )
 
-@app.exception_handler(ValidationException)
-async def validation_exception_handler(request: Request, exc: ValidationException):
-    return JSONResponse(
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        content=jsonable_encoder({"detail": exc.errors()}),
-    )
-
-fastapi_users = FastAPIUsers[User, int](
-    get_user_manager,
-    [auth_backend],
-)
-
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth/jwt",
-    tags=["auth"],
+    prefix="/auth",
+    tags=["Auth"],
 )
 
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
-    tags=["auth"],
+    tags=["Auth"],
 )
 
-current_user = fastapi_users.current_user()
-
-@app.get("/protected-route")
-def protected_route(user: User = Depends(current_user)):
-    return f"Hello, {user.username}"
-
-
-@app.get("/unprotected-route")
-def unprotected_route():
-    return f"Hello, anonym"
+app.include_router(router_operation)
