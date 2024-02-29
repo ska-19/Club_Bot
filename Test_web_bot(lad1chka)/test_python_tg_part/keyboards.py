8647d742fb349422dc237058acb6dd5d84b9b09c
lad1chka <vlad5476357@gmail.com>
2024-02-29 15:14:30 +0300

    update link to github.io and create Web directory

--- Test_web_bot(lad1chka)/test_python_tg_part/keyboards.py
+++ Test_web_bot(lad1chka)/test_python_tg_part/keyboards.py
@@ -1,7 +1,7 @@
 from aiogram.types import WebAppInfo
 from aiogram import types
 
-web_app = WebAppInfo(url='site_url')
+web_app = WebAppInfo(url='https://lad1chka.github.io/')
 
 keyboard = types.ReplyKeyboardMarkup(
     keyboard=[
