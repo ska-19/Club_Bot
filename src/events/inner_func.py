68b96f13a89f16a6c59f7368e8446195302de310
Sensorzxc <sensor.main@gmail.com>
2024-03-27 18:59:32 +0300

    fix all errors events

--- src/events/inner_func.py
+++ src/events/inner_func.py
@@ -3,7 +3,7 @@ from sqlalchemy import select, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database import get_async_session
-from src.events.models import event
+from src.events.models import event, event_reg
 
 error = {
     "status": "error",
@@ -88,9 +88,9 @@ async def check_rec_event(
         session: AsyncSession = Depends(get_async_session)
 ):
     try:
-        query = select(event).where(
-            (event.c.user_id == user_id) &
-            (event.c.id == event_id))
+        query = select(event_reg).where(
+            (event_reg.c.user_id == user_id) &
+            (event_reg.c.id == event_id))
         result = await session.execute(query)
         data = result.mappings().first()
 
@@ -98,5 +98,19 @@ async def check_rec_event(
             return True
         else:
             return False
+    except Exception:
+        raise HTTPException(status_code=500, detail=error)
+
+
+async def get_club_id_by_event_id(
+        event_id: int,
+        session: AsyncSession = Depends(get_async_session)
+):
+    try:
+        query = select(event).where(event.c.id == event_id)
+        result = await session.execute(query)
+        data = result.mappings().first()
+
+        return data['club_id']
     except Exception:
         raise HTTPException(status_code=500, detail=error)
