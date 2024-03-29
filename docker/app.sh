835ec5020ad1c067b1e52ec06ac2590c78270678
ska-19 <ska04@icloud.com>
2024-03-29 22:27:08 +0300

    deploy 0.0.3

--- docker/app.sh
+++ docker/app.sh
@@ -2,6 +2,4 @@
 
 alembic upgrade head
 
-cd src
-
-gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+gunicorn src.main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
