177b09f24481680391e6c4b851f6ecded978d327
lad1chka <vlad5476357@gmail.com>
2024-03-29 15:34:07 +0300

    not worked not_get requests

--- src/pages/router.py
+++ src/pages/router.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter, Request, Depends
+from fastapi import APIRouter, Request, Depends, Form
 from fastapi.templating import Jinja2Templates
 
-from src.user_profile.router import get_user_by_id
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.responses import RedirectResponse
+
+from src.database import get_async_session
+from src.user_profile.  router import get_user_by_id, update_profile
+from src.user_profile.schemas import UserUpdate
 from src.club.router import get_club_by_id
 
 router = APIRouter(
@@ -22,21 +27,36 @@ def get_profile_user(request: Request, user_info=Depends(get_user_by_id)):
     return templates.TemplateResponse("profile_user.html", {"request": request, "user_info": user_info})
 
 
+@router.put("/profile_user/{user_id}")
+async def update_profile_user(
+        user_id: int,
+        update_data: UserUpdate,
+        session: AsyncSession = Depends(get_async_session)):
+    try:
+        await update_profile(user_id, update_data, session)
+        return RedirectResponse(url=f"/pages/profile_user/{user_id}")
+
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
 @router.get("/main_base")
 def get_profile_base(request: Request):
-    return templates.TemplateResponse("profile_base.html", {"request": request})
+    pass
 
 
-@router.get("/main_user/{id}") # тут id из club_user
+@router.get("/main_user/{id}")  # тут id из club_user
 def get_profile_user(request: Request, user_info=Depends()):  # а тут что в депендс?
-    return templates.TemplateResponse("profile_user.html", {"request": request})
+    pass
 
 
 @router.get("/main_base")
 def get_profile_base(request: Request):
-    return templates.TemplateResponse("profile_base.html", {"request": request})
+    pass
 
 
 @router.get("/main_user/{club_id}")
 def get_profile_user(request: Request, user_info=Depends(get_club_by_id)):  # а тут что в депендс?
-    return templates.TemplateResponse("profile_user.html", {"request": request})
+    pass
