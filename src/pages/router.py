2c24b68aaa9ecddc1ffecdb7fb82d9b1d53a9b96
Москвитин Владислав Вадимович <vvmoskvitin@edu.hse.ru>
2024-05-05 21:16:06 +0300

    get func for club_user.html. fix get_user_by_id to get_user

--- src/pages/router.py
+++ src/pages/router.py
@@ -9,7 +9,7 @@ from src.user_profile.router import update_profile
 from src.user_profile.inner_func import get_user_by_id
 from src.user_profile.router import get_user
 from src.user_profile.schemas import UserUpdate
-from src.user_club.router import get_clubs_by_user, get_balance
+from src.user_club.router import get_clubs_by_user, get_balance, get_users_in_club
 from src.user_club.inner_func import get_role
 from src.events.router import get_event_club
 from src.achievement.router import get_achievement_by_user
@@ -22,25 +22,21 @@ router = APIRouter(
 templates = Jinja2Templates(directory="src/templates")
 
 
+# Функции для взаимодействия со страницами профиля
 @router.get("/profile_base")
 def get_profile_base(request: Request):
     return templates.TemplateResponse("profile_base.html", {"request": request})
 
 
-# @router.get("/profile_user/{user_id}")
-# async def get_profile_user(
-#         request: Request,
-#         user_info=Depends(get_user),
-#         session: AsyncSession = Depends(get_async_session)
-# ):
-#     user_data = dict(user_info['data'])
-#     user_data['achievment'] = await get_achievement_by_user(user_data['id'], session)
-#     return templates.TemplateResponse("profile_user.html", {"request": request, "user_info": user_data})
-
-
 @router.get("/profile_user/{user_id}")
-async def get_profile_user(request: Request, user_info=Depends(get_user_by_id)):
-    return templates.TemplateResponse("profile_user.html", {"request": request, "user_info": user_info})
+async def get_profile_user(
+        request: Request,
+        user_info=Depends(get_user),
+        session: AsyncSession = Depends(get_async_session)
+):
+    user_data = dict(user_info['data'])
+    user_data['achievment'] = await get_achievement_by_user(user_data['id'], session)
+    return templates.TemplateResponse("profile_user.html", {"request": request, "user_info": user_data})
 
 
 @router.put("/profile_user/{user_id}")
@@ -58,6 +54,7 @@ async def update_profile_user(
         raise HTTPException(status_code=500, detail=str(e))
 
 
+# Функции для взаимодействия со страницами "Главное"
 @router.get("/main_base")
 def get_main_base(request: Request):
     return templates.TemplateResponse("main_base.html", {"request": request})
@@ -66,13 +63,14 @@ def get_main_base(request: Request):
 @router.get("/main_user/{user_id}")
 async def get_main_user(
         request: Request,
-        user_info=Depends(get_user_by_id),
+        user_info=Depends(get_user),
         session: AsyncSession = Depends(get_async_session)
 ):
-    user_clubs = await get_clubs_by_user(user_info['user_id'], session)
+    user_data = dict(user_info['data'])
+    user_clubs = await get_clubs_by_user(user_data['id'], session)
     club_info = dict(user_clubs['data'][0])
-    user_x_club_info_role = await get_role(user_info['user_id'], club_info['id'], session)
-    user_x_club_info_balance = await get_balance(user_info['user_id'], club_info['id'], session)
+    user_x_club_info_role = await get_role(user_data['id'], club_info['id'], session)
+    user_x_club_info_balance = await get_balance(user_data['id'], club_info['id'], session)
     event_data = await get_event_club(club_info['id'], session)
     event_info = event_data['data']
     events = [dict(event) for event in event_info]
@@ -83,18 +81,34 @@ async def get_main_user(
     }
     return templates.TemplateResponse("main_user.html", {
         "request": request,
-        "user_info": user_info,
+        "user_info": user_data,
         "club_info": club_info,
         "user_x_club_info": user_x_club_info,
         "events": events
     })
 
+
+# Функции для взаимодействия со страницами "О клубе"
 @router.get("/club_base")
 def get_club_base(request: Request):
     return templates.TemplateResponse("club_base.html", {"request": request})
 
 
 @router.get("/club_user/{user_id}")
-def get_club_user(request: Request, user_info=Depends(get_user_by_id)):
-    return templates.TemplateResponse("club_user.html", {"request": request, "user_info": user_info})
-
+async def get_club_user(
+        request: Request,
+        user_info=Depends(get_user),
+        session: AsyncSession = Depends(get_async_session)
+):
+    user_data = dict(user_info['data'])
+    user_clubs = await get_clubs_by_user(user_data['id'], session)
+    club_info = dict(user_clubs['data'][0])
+    users_in_club = await get_users_in_club(club_info['id'], session)
+    users = users_in_club['data']
+    club_info['xp'] = 0
+    return templates.TemplateResponse("club_user.html", {
+        "request": request,
+        "user_info": user_data,
+        "club_info": club_info,
+        "users": users
+    })
