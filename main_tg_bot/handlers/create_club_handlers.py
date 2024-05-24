924cc49b7a99ddc19c73063dbd19a0fe4f953232
Семенкович Кирилл Александрович <kasemenkovich@edu.hse.ru>
2024-05-24 16:02:14 +0300

    main_tg_bot 0.0

---
+++ main_tg_bot/handlers/create_club_handlers.py
@@ -0,0 +1,71 @@
+from aiogram import Router, F
+from aiogram.filters import Command, StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import Message, ReplyKeyboardRemove
+
+from keyboards.simple_kb import make_colum_keyboard
+from keyboards.user_keyboards import get_main_kb, get_back_button
+
+from database import requests as rq
+
+router = Router()
+class CreateClub(StatesGroup):
+    enter_name = State()
+    enter_dest = State()
+    enter_bio = State()
+    enter_link_channel = State()
+
+@router.callback_query(F.data == "create_club")
+async def cmd_create(message: Message, state: FSMContext):
+    await message.answer(
+        text="🎩 <b>Давайте создадим клуб</b>\n\n"
+             "Для начала укажите название:",
+        reply_markup=get_back_button()
+    )
+    await rq.set_user(message.from_user.id)
+    await state.set_state(CreateClub.enter_name)
+
+
+
+@router.message(StateFilter("HobbiesQuest:choosing_hobbies"))
+async def quest_chosen_incorrectly_hobbies(message: Message):
+    await message.answer(
+        text="😭 Я не понимаю такого ответа.\n\nПожалуйста, выберите один из вариантов "
+             "из списка:",
+    )
+
+@router.message(StateFilter("HobbiesQuest:tell_expectations"))
+async def quest_tell_expectations_incorrectly(message: Message):
+    await message.answer(
+        text="😇 Пожалуйста, опишите ваши ожидания от клуба, немного подробней!",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+#
+# @router.message(HobbiesQuest.choosing_stay_in_touch, F.text.in_(available_stay_in_touch))
+# async def quest_chosen(message: Message, state: FSMContext):
+#     # await state.update_data(chosen_stay_in_touch=message.text.lower())
+#     chosen_stay_in_touch = message.text.lower()
+#     for i in range(len(available_stay_in_touch)):
+#         if available_stay_in_touch[i].lower() == chosen_stay_in_touch:
+#             await state.update_data(chosen_stay_in_touch=i)
+#     user_data = await state.get_data()
+#     await message.answer(
+#         text="🎉 <b>Вы прошли все вопросы! Спасибо за ответы!</b> ❤️\n\n"
+#              "Создателям очень важно собрать как можно больше ответов, чтобы воплотить в жизнь свой замысел!\n\n"
+#              "Вы можете помочь нам! Заодно поучаствовать в <b>розыгрыше призов</b>, опросив своего хорошего друга!\n"
+#              "Ещё раз нажав на /quest\n\n\n"
+#              "p.s. Как вы могли заметить вопросы простые и их не много)\n"
+#              "Уверены, что вы знаете увлечения своих друзей "
+#              "и сможете заполнить анкету ещё раз, даже если их нет рядом.",
+#
+#         reply_markup=get_main_kb()
+#     )
+#     await rq.set_user_tell_questionnaire(message.from_user.id, user_data),
+#     await state.clear()
+
+
+# @router.message(HobbiesQuest.choosing_stay_in_touch)
+# async def quest_chosen_incorrectly(message: Message):
+#     await send_error_message(message, available_stay_in_touch)
---
+++ main_tg_bot/keyboards/user_keyboards.py
@@ -0,0 +1,22 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import WebAppInfo
+
+def get_main_ikb(tg_id: int = 0) -> InlineKeyboardMarkup:
+    """Get main keyboard."""
+    web_app = WebAppInfo(url=f'http://club-bot.onrender.com/pages/profile_user/{tg_id}')
+    ikb = [
+        [InlineKeyboardButton(text='ЗАПУСТИТЬ', web_app=web_app)]
+        [InlineKeyboardButton(text='Создать клуб', callback_data='create_club')],
+
+    ]
+    ikeyboard = InlineKeyboardMarkup(inline_keyboard=ikb)
+    return ikeyboard
+
+def get_back_button() -> InlineKeyboardMarkup:
+    """Get back button."""
+    ikb = [
+        [InlineKeyboardButton(text="Назад", callback_data="back")],
+    ]
+    ikeyboard = InlineKeyboardMarkup(inline_keyboard=ikb)
+    return ikeyboard
