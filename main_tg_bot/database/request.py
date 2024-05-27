d2e7270f1af5babaffe1da19376a638360ce49e7
Семенкович Кирилл Александрович <kasemenkovich@edu.hse.ru>
2024-05-27 18:45:58 +0300

    main_tg_bot 0.3

--- main_tg_bot/database/request.py
+++ main_tg_bot/database/request.py
@@ -13,8 +13,8 @@ async def set_user(user_data):
     rq_user_data = {
         "id": int(user_data["tg_id"]),
         "username": str(user_data["username"]),
-        "name": str(user_data["name"]),
-        "surname": str(user_data["surname"]),
+        "name": '' if user_data["name"] is None or 'None' else str(user_data["name"]),
+        "surname": '' if user_data["surname"] is None or 'None' else str(user_data["surname"]),
     }
 
     headers = {
@@ -23,10 +23,15 @@ async def set_user(user_data):
     url = str(URL)+'/user_profile/create_user'
 
     response = requests.post(url, json=rq_user_data, headers=headers)
+
+    # if response.status_code == 409:
+    #     response = requests.post(str(URL)+'/user_profile/update_user', json=rq_user_data, headers=headers)
     return response
 
 
 async def set_club(tg_id, club_data):
+    if club_data['channel_link'][0] == '@':
+        club_data['channel_link'] = 'https://t.me/'+club_data['channel_link'][1:]
 
     rq_club_data = {
         "owner": int(tg_id),
