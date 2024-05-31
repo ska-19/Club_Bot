106542d0050405f7abd10c2b15b185617bb6e0e4
Соснин Тимофей Алексеевич <tasosnin@edu.hse.ru>
2024-05-31 05:34:19 +0300

    fix and upgrade market

--- src/market/schemas.py
+++ src/market/schemas.py
@@ -6,17 +6,18 @@ from pydantic import BaseModel
 class AddProduct(BaseModel):
     name: str
     price: int
+    user_id: int
     description: str
     quantity: int
-    admin_id: int
     club_id: int
 
 
 class UpdateProduct(BaseModel):
+    id: int
+    user_id: int
     name: str
     price: int
     description: str
     quantity: int
-    admin_id: int
 
 
